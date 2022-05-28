cd77d484ad2f5f5338e2a213ed573460dc3fec89
MdZillurRahman <mzisanr@gmail.com>
2022-05-28 11:02:22 +0600

    Add Review done

--- src/Components/Dashboard/Review.js
+++ src/Components/Dashboard/Review.js
@@ -5,79 +5,79 @@ import { toast } from 'react-toastify';
 const Review = () => {
     const { register, handleSubmit } = useForm();
 
-    // const onSubmit = data => {
+    const onSubmit = data => {
 
-    //     const url = 'http://localhost:5000/reviews';
-    //     fetch(url, {
-    //         method: 'POST',
-    //         headers: {
-    //             'content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify(data)
-    //     })
-    //         .then(res => res.json())
-    //         .then(result => {
-    //             console.log(result);
-    //             toast('Review Added');
-    //         })
-    // };
+        const url = 'http://localhost:5000/reviews';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+                toast('Review Added');
+            })
+    };
 
-    // return (
-    //     <div>
-    //         <h2 className='text-2xl text-center'>Add A Review</h2>
-    //         <form className='flex flex-col justify-center' onSubmit={handleSubmit(onSubmit)}>
-    //             <div class="form-control w-full max-w-xs">
-    //                 <label class="label">
-    //                     <span class="label-text">Name</span>
-    //                 </label>
-    //                 <input type="text" placeholder="Name" class="input input-bordered w-full max-w-xs" {...register("name", { required: true, maxLength: 20 })} />
-    //             </div>
+    return (
+        <div>
+            <h2 className='text-2xl text-center'>Add A Review</h2>
+            <form className='flex flex-col justify-center' onSubmit={handleSubmit(onSubmit)}>
+                <div class="form-control w-full max-w-xs">
+                    <label class="label">
+                        <span class="label-text">Name</span>
+                    </label>
+                    <input type="text" placeholder="Name" class="input input-bordered w-full max-w-xs" {...register("name", { required: true, maxLength: 20 })} />
+                </div>
 
-    //             <div class="form-control w-full max-w-xs">
-    //                 <label class="label">
-    //                     <span class="label-text">Tool Name</span>
-    //                 </label>
-    //                 <input type="" placeholder='Tool Name' class="input input-bordered w-full max-w-xs" {...register("review",
-    //                     {
-    //                         required: {
-    //                             value: true
-    //                         }
-    //                     }
-    //                 )} />
-    //             </div>
+                <div class="form-control w-full max-w-xs">
+                    <label class="label">
+                        <span class="label-text">Tool Name</span>
+                    </label>
+                    <input type="" placeholder='Tool Name' class="input input-bordered w-full max-w-xs" {...register("review",
+                        {
+                            required: {
+                                value: true
+                            }
+                        }
+                    )} />
+                </div>
 
-    //             <div class="form-control w-full max-w-xs">
-    //                 <label class="label">
-    //                     <span class="label-text">Location</span>
-    //                 </label>
-    //                 <input type="text" placeholder="Location" class="input input-bordered w-full max-w-xs" {...register("location", {
-    //                     required: {
-    //                         value: true
-    //                     }
-    //                 })} />
-    //             </div>
+                <div class="form-control w-full max-w-xs">
+                    <label class="label">
+                        <span class="label-text">Location</span>
+                    </label>
+                    <input type="text" placeholder="Location" class="input input-bordered w-full max-w-xs" {...register("location", {
+                        required: {
+                            value: true
+                        }
+                    })} />
+                </div>
 
-    //             <div class="form-control w-full max-w-xs">
-    //                 <label class="label">
-    //                     <span class="label-text">Description</span>
-    //                 </label>
-    //                 <input type="text" placeholder="Tool Description" class="input input-bordered w-full max-w-xs" {...register("description", {
-    //                     required: {
-    //                         value: true
-    //                     }   
-    //                 })} />
-    //             </div>
+                <div class="form-control w-full max-w-xs">
+                    <label class="label">
+                        <span class="label-text">Description</span>
+                    </label>
+                    <input type="text" placeholder="Tool Description" class="input input-bordered w-full max-w-xs" {...register("description", {
+                        required: {
+                            value: true
+                        }   
+                    })} />
+                </div>
 
-    //             <div class="form-control w-full max-w-xs">
-    //                 <label class="label">
-    //                     <span class="label-text">Image URL</span>
-    //                 </label>
-    //                 <input type="url" placeholder="url" class="input input-bordered w-full max-w-xs" {...register("img", { required: true })} />
-    //             </div>
-    //             <button type='submit' class="btn btn-sm w-24 mt-8">Add Review</button>
+                <div class="form-control w-full max-w-xs">
+                    <label class="label">
+                        <span class="label-text">Image URL</span>
+                    </label>
+                    <input type="url" placeholder="url" class="input input-bordered w-full max-w-xs" {...register("img", { required: true })} />
+                </div>
+                <button type='submit' class="btn btn-sm w-24 mt-8">Add Review</button>
 
-    //         </form>
-    //     </div>
+            </form>
+        </div>
     );
 };
 
